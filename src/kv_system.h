0d53d1bd307ff8356bcd32f23d3871e926faad9e
sfazackerley <scottfaz@gmail.com>
2014-06-03 22:38:26 -0700

    Changed system.h to kv_system.h.  Other minor edits to allow for use with makefile project in eclipse

---
+++ src/kv_system.h
@@ -0,0 +1,42 @@
+/*
+ * System.h
+ *
+ *  Created on: May 20, 2014
+ *      Author: workstation
+ */
+
+#ifndef SYSTEM_H_
+#define SYSTEM_H_
+
+#define USING_ECLIPSE 1
+#define DEBUG 1
+#define IS_EQUAL 0
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+enum status
+{
+	status_ok,
+	status_item_not_found,
+	status_duplicate_key,
+};
+
+typedef char status_t;
+
+enum error
+{
+	err_ok = 0,
+	err_item_not_found,
+	err_duplicate_key,
+	err_max_capacity,
+	err_write_concern,
+	err_colllection_destruction_error,
+};
+
+typedef char err_t;
+
+
+#endif /* SYSTEM_H_ */
