66a0ec46f74656ef43ca1a424b5a1aa28f69f898
Graeme Douglas <gdouglas.dev@gmail.com>
2016-06-02 11:48:59 -0700

    Turn on all formatting

--- dotfiles/scripts/format.py
+++ dotfiles/scripts/format.py
@@ -14,5 +14,5 @@ files		= subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)\
 print files
 for fname in files.split():
 	#print(fname)
-	#os.system("uncrustify -c dotfiles/iondb_style.cfg --replace --no-backup " + fname)
+	os.system("uncrustify -c dotfiles/iondb_style.cfg --replace --no-backup " + fname)
 	subprocess.call(["python", "dotfiles/scripts/spacestotabs.py", fname, fname])
