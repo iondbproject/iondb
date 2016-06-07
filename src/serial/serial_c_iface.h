f14513747392696672339ff2a84e159dd291337c
Graeme Douglas <gdouglas.dev@gmail.com>
2016-06-07 11:51:05 -0700

    Add and fix documentation in serial/util code
    Clean up sentence structure, add missing information, add missing
    documentation, add license info.

--- src/serial/serial_c_iface.h
+++ src/serial/serial_c_iface.h
@@ -1,11 +1,26 @@
 /******************************************************************************/
 /**
-@file		serial_c_iface.h
+@file
 @author		Scott Fazackerley
-@brief		Proivdes a printf implementaton for c code running from inside an
+@brief		Provides a printf implementaton for C code running from inside an
 			Arduino Sketch.
 
-			IMPORTANT: This must be included after stdio.h to function properly
+			IMPORTANT: This must be included after stdio.h to function properly.
+@copyright	Copyright 2016
+				The University of British Columbia,
+				IonDB Project Contributors (see @ref AUTHORS.md)
+@par
+			Licensed under the Apache License, Version 2.0 (the "License");
+			you may not use this file except in compliance with the License.
+			You may obtain a copy of the License at
+					http://www.apache.org/licenses/LICENSE-2.0
+@par
+			Unless required by applicable law or agreed to in writing,
+			software distributed under the License is distributed on an
+			"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+			either express or implied. See the License for the specific
+			language governing permissions and limitations under the
+			License.
 */
 /******************************************************************************/
 
@@ -18,26 +33,35 @@
 extern "C" {
 #endif
 
-#define USE_INLINEPRINTF 0	/** <setting this value to 1 will use an inline replacement for printf
-								which reduces call stack requirements (minimal) but increased code
-								footprint considerably for each use of printf.  You decide if you
-								want speed vs. minimal code space */
+/**
+@brief		Setting this value to 1 will use an inline replacement for printf
+			which reduces call stack requirements (minimal) but increase code
+			footprint considerably for each use of printf. You decide if you
+			want speed vs. minimal code space.
+*/
+#define USE_INLINEPRINTF 0
 
 
 
 /**
- @brief		Overload of printf for arduino to allow a limited use printf
- @param 	format
+@brief		A version of printf that limits the number of characters
+			printed per call.
+@details	When additional characters are requested for printing, the
+			print fails.
+@param	 	format
  	 	 	 	 The string that contains the text to be written to serial.
- @return 	The number of characters written.  Returns a negative value on failure.
- */
+@param		...
+				Variable arguments of data used by the format.
+@return 	The number of characters written. Returns a negative value on
+			failure.
+*/
 int
 serial_printf_c(
 	const char 	*format,
 	...
 );
 
-/** DO NOT CHANGE INCLUSION ORDER */
+/* DO NOT CHANGE INCLUSION ORDER. */
 #if USE_INLINE_PRINTF == 1
 	#include "printf_redirect.h"
 #else
@@ -45,10 +69,10 @@ serial_printf_c(
 #endif
 
 /**
-@brief 		Wrapper to Serial stream
+@brief 		A print function wrapping Arduino's serial stream.
 @param		buffer
-				pointer to buffer to print
-@return		The number of characters outputted
+				Pointer to the character array / buffer to print.
+@return		The number of characters outputted.
 */
 extern int
 serial_print(
@@ -57,8 +81,8 @@ serial_print(
 
 
 /**
-@brief		Initializes serial port 0 for communications.  By default
- 	 	 	the port is set up at N-8-1.
+@brief		Initializes serial port 0 for communications.
+@details	By default the port is set up at N-8-1.
 @param 		baud_rate
 				The buad rate to be used.
 */
@@ -68,8 +92,9 @@ serial_init(
 );
 
 /**
-@brief		Closes the comm port so that the pins can be used as general i/o.
- */
+@brief		Closes the communication port so that the pins can be used as
+			general I/O.
+*/
 void
 serial_close(
 );
