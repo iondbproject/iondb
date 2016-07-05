11e1914666d6fdc666e167c4dfecc9914910bf73
Kris Wallperington <Honibannchi@gmail.com>
2016-07-05 14:37:18 -0700

    Fix missing SD begin in C++ Wrapper test

--- src/tests/unit/cpp_wrapper/cpp_wrapper.ino
+++ src/tests/unit/cpp_wrapper/cpp_wrapper.ino
@@ -1,10 +1,14 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <SD.h>
 #include "test_cpp_wrapper.h"
 
 void
 setup(
 )
 {
+    SPI.begin();
+    SD.begin(SD_CS_PIN);
     Serial.begin(BAUD_RATE);
     runalltests_cpp_wrapper();
 }
