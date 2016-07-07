8d0a39640989a7d146223a3190186064d57ab71a
Wade Penson <wadepenson@gmail.com>
2016-07-07 11:31:56 -0700

    Fix iframes for svg images

--- documentation/doxygen/misc.js
+++ documentation/doxygen/misc.js
@@ -10,42 +10,7 @@ $( document ).ready(function() {
 		icons: {"header": "ui-icon-plus", "activeHeader": "ui-icon-minus"}
 	});
 
-	// // $('.table td, .table tr, .table th').has('groupheader').css('border-top', 'none');
-
-	// // var svg_buttons = '<div class="buttons">' +
-	// // 	'<button class="zoom-in">Zoom In</button>' +
-	// // 	'<button class="zoom-out">Zoom Out</button>' +
-	// // 	'<input type="range" class="zoom-range">' +
-	// // 	'<button class="reset">Reset</button></div>';
-
-	var iframe = $('.dyncontent iframe');
-
-	iframe.each(function() {
-		content = $(this).contents().find('svg');
-		$(this).before('<a href="' + content.attr('src') + '"><span class="glyphicon glyphicon-file" aria-hidden="true"></span></a>');
-		$(this).hide();
-	});
-
 	$(window).resize(function() {
 		$(window).width();
 	});
-
-	// if (is_cross_site_safe) {
-	// 	iframe.remove();
-
-	// 	// var svg = $('svg');
-	//  //    svg.parent().panzoom({
-	//  //        $zoomIn: svg.find(".zoom-in"),
-	//  //        $zoomOut: svg.find(".zoom-out"),
-	//  //        $zoomRange: svg.find(".zoom-range"),
-	//  //        $reset: svg.find(".reset"),
-	//  //        startTransform: 'scale(0.9)',
-	//  //        maxScale: 20,
-	//  //        increment: 0.1,
-	//  //        contain: true
- //  //   	}).panzoom('zoom', true);
-	// }
-	// else {
-	// 	iframe.width('100%');
-	// }
 });
