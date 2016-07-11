37ff15d53624f8b19e47b279a56a13679d5643e2
Kris Wallperington <Honibannchi@gmail.com>
2016-07-11 15:30:00 -0700

    Add entry point IonDB.h

---
+++ src/IonDB.h
@@ -0,0 +1,38 @@
+/******************************************************************************/
+/**
+@file
+@author		Kris Wallperington
+@brief		Entry point for IonDB.
+@copyright	Copyright 2016
+				The University of British Columbia,
+				IonDB Project Contributors (see AUTHORS.md)
+@par
+			Licensed under the Apache License, Version 2.0 (the "License");
+			you may not use this file except in compliance with the License.
+			You may obtain a copy of the License at
+					http://www.apache.org/licenses/LICENSE-2.0
+@par
+			Unless required by applicable law or agreed to in writing,
+			software distributed under the License is distributed on an
+			"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+			either express or implied. See the License for the specific
+			language governing permissions and limitations under the
+			License.
+*/
+/******************************************************************************/
+
+#if !defined(IONDB_H)
+#define IONDB_H
+
+#include "key_value/kv_system.h"
+#include "key_value/kv_io.h"
+#include "cpp_wrapper/Dictionary.h"
+#include "cpp_wrapper/FlatFile.h"
+#include "cpp_wrapper/OpenAddressFileHash.h"
+#include "cpp_wrapper/OpenAddressHash.h"
+#include "cpp_wrapper/BppTree.h"
+#include "cpp_wrapper/SkipList.h"
+
+#include "serial/serial_c_iface.h"
+
+#endif
