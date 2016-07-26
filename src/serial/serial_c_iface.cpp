ff8d5a21d986f7ba8072021f932d2051d07231c4
Kris Wallperington <Honibannchi@gmail.com>
2016-07-25 18:15:56 -0700

    Fix off-by-one in vsnprintf

--- src/serial/serial_c_iface.cpp
+++ src/serial/serial_c_iface.cpp
@@ -33,8 +33,9 @@ serial_printf_c(
 
 	va_start(args, format);
 
-	int		bufsize = vsnprintf(NULL, 0, format, args);
-	char	buf[bufsize];
+	int bufsize = vsnprintf(NULL, 0, format, args);
+	/* +1 for the null terminator \0 at the end */
+	char buf[bufsize + 1];
 
 	va_end(args);
 
