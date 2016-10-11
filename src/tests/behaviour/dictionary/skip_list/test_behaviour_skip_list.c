3db1fcf4fb9a89f8458a69f34609bd27fd3648de
Spencer MacBeth <spencermacbeth@Spencers-MacBook-Pro.local>
2016-10-11 16:48:35 -0700

    fixed updated references to BHDCT_* to ION_BHDCT_* in test_behaviour_skip_list.c

--- src/tests/behaviour/dictionary/skip_list/test_behaviour_skip_list.c
+++ src/tests/behaviour/dictionary/skip_list/test_behaviour_skip_list.c
@@ -31,7 +31,7 @@ runalltests_behaviour_skip_list(
 	void
 ) {
 #if defined(ARDUINO)
-	bhdct_run_tests(sldict_init, 7, BHDCT_ALL_TESTS & ~BHDCT_STRING_INT);
+	bhdct_run_tests(sldict_init, 7, ION_BHDCT_ALL_TESTS & ~ION_BHDCT_STRING_INT);
 #else
 	bhdct_run_tests(sldict_init, 7, ION_BHDCT_ALL_TESTS);
 #endif
