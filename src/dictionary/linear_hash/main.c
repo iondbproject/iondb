330a295f2a3b15c4343f3a88e5dc40ac86df5238
Spencer MacBeth <spencermacbeth@Spencers-MacBook-Pro.local>
2017-02-06 18:40:42 -0800

    removed commented out tests

--- src/dictionary/linear_hash/main.c
+++ src/dictionary/linear_hash/main.c
@@ -32,8 +32,6 @@ int main() {
     linear_hash_insert(15, insert_hash_to_bucket(15, linear_hash), linear_hash);
     linear_hash_insert(5, insert_hash_to_bucket(5, linear_hash), linear_hash);
     linear_hash_insert(20, insert_hash_to_bucket(25, linear_hash), linear_hash);
-//    linear_hash_delete(20, linear_hash);
-//    linear_hash_delete(15, linear_hash);
 
     linear_hash_insert(15, insert_hash_to_bucket(15, linear_hash), linear_hash);
     linear_hash_insert(10, insert_hash_to_bucket(10, linear_hash), linear_hash);
@@ -43,58 +41,13 @@ int main() {
     linear_hash_insert(15, insert_hash_to_bucket(15, linear_hash), linear_hash);
     linear_hash_insert(10, insert_hash_to_bucket(10, linear_hash), linear_hash);
     linear_hash_insert(15, insert_hash_to_bucket(15, linear_hash), linear_hash);
-//    linear_hash_insert(20, insert_hash_to_bucket(20, linear_hash), linear_hash);
-//
-//    linear_hash_insert(10, insert_hash_to_bucket(10, linear_hash), linear_hash);
-//    linear_hash_insert(15, insert_hash_to_bucket(15, linear_hash), linear_hash);
-//    linear_hash_insert(20, insert_hash_to_bucket(20, linear_hash), linear_hash);
-//    linear_hash_insert(10, insert_hash_to_bucket(10, linear_hash), linear_hash);
-////
-//    linear_hash_insert(15, insert_hash_to_bucket(15, linear_hash), linear_hash);
-//    linear_hash_insert(10, insert_hash_to_bucket(10, linear_hash), linear_hash);
-//    linear_hash_insert(2, insert_hash_to_bucket(2, linear_hash), linear_hash);
-//    linear_hash_insert(7, insert_hash_to_bucket(7, linear_hash), linear_hash);
-//    linear_hash_insert(7, insert_hash_to_bucket(7, linear_hash), linear_hash);
-//    linear_hash_insert(17, insert_hash_to_bucket(17, linear_hash), linear_hash);
-//
-//    linear_hash_insert(2, insert_hash_to_bucket(2, linear_hash), linear_hash);
-//    linear_hash_insert(12, insert_hash_to_bucket(12, linear_hash), linear_hash);
-//    linear_hash_insert(2, insert_hash_to_bucket(2, linear_hash), linear_hash);
-//    linear_hash_insert(12, insert_hash_to_bucket(12, linear_hash), linear_hash);
-//
-//    linear_hash_insert(14, insert_hash_to_bucket(14, linear_hash), linear_hash);
-//    linear_hash_insert(14, insert_hash_to_bucket(14, linear_hash), linear_hash);
-//    linear_hash_insert(14, insert_hash_to_bucket(14, linear_hash), linear_hash);
-//    linear_hash_insert(14, insert_hash_to_bucket(14, linear_hash), linear_hash);
-//
-//    linear_hash_insert(13, insert_hash_to_bucket(13, linear_hash), linear_hash);
-//    linear_hash_insert(18, insert_hash_to_bucket(18, linear_hash), linear_hash);
-//    linear_hash_insert(13, insert_hash_to_bucket(13, linear_hash), linear_hash);
-//    linear_hash_insert(23, insert_hash_to_bucket(23, linear_hash), linear_hash);
 
-    //printf("\nPERFORMING GETS\n");
-    //print_linear_hash_bucket_from_idx(0, linear_hash);
-//    linear_hash_get(13, linear_hash);
-//    linear_hash_get(15, linear_hash);
-//    linear_hash_get(2, linear_hash);
-    //linear_hash_get(35, linear_hash);    printf("\nPERFORMING GETS\n");
+
     printf("\nPRINT BUCKET 0\n");
     print_all_linear_hash_index_buckets(0, linear_hash);
 
-//    printf("\nPRINT BUCKET 5\n");
+    printf("\nPRINT BUCKET 5\n");
     print_all_linear_hash_index_buckets(5, linear_hash);
-
-//
-//    printf("\nGET 20\n");
-//    linear_hash_get(20, linear_hash);
-
-//    printf("\nGET 10\n");
-//    linear_hash_get(10, linear_hash);
-//
-//    linear_hash_get(13, linear_hash);
-//    linear_hash_get(10, linear_hash);
-//    linear_hash_get(2, linear_hash);
-//    linear_hash_get(17, linear_hash);
 
     return 0;
 }
