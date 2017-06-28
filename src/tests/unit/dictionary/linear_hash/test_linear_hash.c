084f5431f44688ebe72334c96094e97adb59977c
danaack <danak@shaw.ca>
2017-06-28 13:47:54 -0700

    Fix linear hash device build

--- src/tests/unit/dictionary/linear_hash/test_linear_hash.c
+++ src/tests/unit/dictionary/linear_hash/test_linear_hash.c
@@ -477,8 +477,8 @@ linear_hash_getsuite_1(
 ) {
 	planck_unit_suite_t *suite = planck_unit_new_suite();
 
-	PLANCK_UNIT_ADD_TO_SUITE(suite, test_linear_hash_create_destroy);
-//	PLANCK_UNIT_ADD_TO_SUITE(suite, test_linear_hash_basic_operations);
+//	PLANCK_UNIT_ADD_TO_SUITE(suite, test_linear_hash_create_destroy);
+	PLANCK_UNIT_ADD_TO_SUITE(suite, test_linear_hash_basic_operations);
 //	PLANCK_UNIT_ADD_TO_SUITE(suite, test_linear_hash_bucket_map_head_updates);
 //	PLANCK_UNIT_ADD_TO_SUITE(suite, test_linear_hash_increment_buckets);
 	return suite;
