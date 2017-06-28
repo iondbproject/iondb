63b6b44d3afb960a485662f24a0041cc376c213d
danaack <danak@shaw.ca>
2017-06-28 13:20:57 -0700

    Fix device-build

--- src/tests/unit/cpp_wrapper/test_cpp_wrapper.cpp
+++ src/tests/unit/cpp_wrapper/test_cpp_wrapper.cpp
@@ -980,9 +980,9 @@ test_cpp_wrapper_get_nonexist_single_all(
 	test_cpp_wrapper_get_nonexist_single(tc, dict);
 	delete dict;
 
-//	dict = new LinearHash<int, int>(0, key_type_numeric_signed, sizeof(int), sizeof(int), 50);
-//	test_cpp_wrapper_get_nonexist_single(tc, dict);
-//	delete dict;
+	dict = new LinearHash<int, int>(0, key_type_numeric_signed, sizeof(int), sizeof(int), 50);
+	test_cpp_wrapper_get_nonexist_single(tc, dict);
+	delete dict;
 }
 
 /**
@@ -1028,9 +1028,9 @@ test_cpp_wrapper_get_nonexist_many_all(
 	test_cpp_wrapper_get_nonexist_many(tc, dict);
 	delete dict;
 
-//	dict = new LinearHash<int, int>(0, key_type_numeric_signed, sizeof(int), sizeof(int), 50);
-//	test_cpp_wrapper_get_nonexist_many(tc, dict);
-//	delete dict;
+	dict = new LinearHash<int, int>(0, key_type_numeric_signed, sizeof(int), sizeof(int), 50);
+	test_cpp_wrapper_get_nonexist_many(tc, dict);
+	delete dict;
 }
 
 /**
@@ -3186,6 +3186,19 @@ cpp_wrapper_getsuite_1(
 	PLANCK_UNIT_ADD_TO_SUITE(suite, test_cpp_wrapper_get_populated_single_all);
 	PLANCK_UNIT_ADD_TO_SUITE(suite, test_cpp_wrapper_get_populated_multiple_all);
 	PLANCK_UNIT_ADD_TO_SUITE(suite, test_cpp_wrapper_get_all_all);
+
+	return suite;
+}
+
+/**
+@brief		Creates the suite to test.
+@return		Pointer to a test suite.
+*/
+planck_unit_suite_t *
+cpp_wrapper_getsuite_2(
+) {
+	planck_unit_suite_t *suite = planck_unit_new_suite();
+
 	PLANCK_UNIT_ADD_TO_SUITE(suite, test_cpp_wrapper_delete_empty_all);
 	PLANCK_UNIT_ADD_TO_SUITE(suite, test_cpp_wrapper_delete_nonexist_single_all);
 	PLANCK_UNIT_ADD_TO_SUITE(suite, test_cpp_wrapper_delete_nonexist_several_all);
@@ -3208,7 +3221,7 @@ cpp_wrapper_getsuite_1(
 @return		Pointer to a test suite.
 */
 planck_unit_suite_t *
-cpp_wrapper_getsuite_2(
+cpp_wrapper_getsuite_3(
 ) {
 	planck_unit_suite_t *suite = planck_unit_new_suite();
 
@@ -3236,7 +3249,7 @@ cpp_wrapper_getsuite_2(
 @return		Pointer to a test suite.
 */
 planck_unit_suite_t *
-cpp_wrapper_getsuite_3(
+cpp_wrapper_getsuite_4(
 ) {
 	planck_unit_suite_t *suite = planck_unit_new_suite();
 
@@ -3274,4 +3287,9 @@ runalltests_cpp_wrapper(
 
 	planck_unit_run_suite(suite3);
 	planck_unit_destroy_suite(suite3);
+
+	planck_unit_suite_t *suite4 = cpp_wrapper_getsuite_4();
+
+	planck_unit_run_suite(suite4);
+	planck_unit_destroy_suite(suite4);
 }
